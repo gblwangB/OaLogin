f981dbbd78e85607ffa2ef61f7674b9a5d2a1463
wangyang <darkstxf@gmail.com>
2018-10-30 14:48:07 +0800

    password Edit

--- jwlogin/jwlogin.py
+++ jwlogin/jwlogin.py
@@ -1,10 +1,10 @@
 #encoding=utf8
 import requests
-### 登录页的url
+###
 url = 'https://sso5x.laosiji.com/sso/login?clientid=9998'
 ### 登录需要提交的表单
-form_data = {'username':'18503718321', #填入网站的上网帐号
-    'password':'1122080!@',  #填入网站密码（加密后的）
+form_data = {'username':'18503711', #填入网站的上网帐号
+              'password':'11220',  #填入网站密码（加密后的）
     }
 s = requests.session()
 response = s.post(url,data = form_data,)
